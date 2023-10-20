b0d63f9cb2ac9df8a110e19e953858900df6e57d
johnmary1 <johnmary1@gmail.com>
2023-10-19 22:14:06 -0400

    Adds two new routes and methods. One renders JSON and one renders html.

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,16 @@ class ExamplePagesController < ApplicationController
   def hello_method
     render json: { message: "Hello! Welcome to my app!" }
   end
+
+  def weather_method
+    render json: { message: "Looks like it's gonna be a sunny day!" }
+  end
+
+  def rendering_html
+    render html: helpers.tag.strong("TRYING SOMETHING DIFFERENT")
+  end
+
+  # def more_difficult_method
+
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/hello_path", controller: "example_pages", action: "hello_method"
-  get "/tiger", controller: "example_pages"
-
+  get "/weather", controller: "example_pages", action: "weather_method"
+  get "/hello_world", controller: "example_pages", action: "rendering_html"
+  # get "/more_difficult", controller: "example_pages", action: "more_difficult_method"
 end
